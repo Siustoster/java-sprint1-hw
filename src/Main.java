b67adc20d6444ed2ea32dbae70aba5179ce69e0c
Siustoster <gameover666999@yandex.ru>
2023-09-13 15:43:13 +0300

    Created method printMenu

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            printMenu();
             int i = scanner.nextInt();
         }
+    }
+    public static void printMenu()
+    {
+        System.out.println("Выберите действие");
+        System.out.println("1 - Ввести количество шагов за определённый день");
+        System.out.println("2 - Изменить цель по количеству шагов в день");
+        System.out.println("3 - Напечатать статистику за определённый месяц");
+        System.out.println("0 - Выход");
+
     }
 }
